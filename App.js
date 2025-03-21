bfcdd5a2a8e287149410a3f173eb969a80e16c9d
Spica-blue <heo1356@gmail.com>
2025-03-22 02:12:55 +0900

    kakao login 테스트 성공

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import OnboardingScreen from './screens/OnboardingScreen';
+import KakaoLogin from "./components/KakaoLogin";
+import { initializeKakaoSDK } from '@react-native-kakao/core';
+import NaverLogin from './components/NaverLogin';
+
+initializeKakaoSDK('ac08cd35c42272ec7b5586c128239a4f');
 
 export default function App() {
 
   return (
     <View style={styles.container}>
       {/* <HomeScreen /> */}
-      <OnboardingScreen />
+      {/* <OnboardingScreen /> */}
+      <KakaoLogin />
+      {/* <NaverLogin /> */}
       <StatusBar style="auto" />
     </View>
   );
